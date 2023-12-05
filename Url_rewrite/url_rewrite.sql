bec5f2cb09622142a4110365b7377f4556dd0eda
Rakibsayyed <124577585+Rakibsayyed@users.noreply.github.com>
2023-12-05 16:42:35 +0530

    Update url_rewrite.sql

--- Url_rewrite/url_rewrite.sql
+++ Url_rewrite/url_rewrite.sql
@@ -1,1 +1,2 @@
-
+/* Delete a few product URLS from URL table. Now write a Query to fetch all the products which do not have entry in url_rewrite using a join */
+SELECT cpe.sku, cpe.entity_id, ur.request_path FROM catalog_product_entity AS cpe JOIN url_rewrite AS ur ON cpe.entity_id = ur.entity_id WHERE ur.request_path IS NULL; 
